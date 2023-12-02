bf59bd99a17e0742ce3ebdd25f8f26ee440ae92e
Timur Akhmadullin <trakhmadullin@edu.hse.ru>
2023-12-02 19:14:14 +0300

    Added css styling and image search page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 window.addEventListener('load', () => {
-  var now = new Date();
+  let now = new Date();
   now.setMinutes(now.getMinutes() - now.getTimezoneOffset())
 
-  /* remove second/millisecond if needed - credit ref. https://stackoverflow.com/questions/24468518/html5-input-datetime-local-default-value-of-today-and-current-time#comment112871765_60884408 */
   now.setMilliseconds(null)
   now.setSeconds(null)
 
